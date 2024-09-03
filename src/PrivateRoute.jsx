e0432da6bcbff50e6468bc041571923777938f44
KaviyaR264 <135967398+KaviyaR264@users.noreply.github.com>
2024-09-03 21:54:59 +0530

    Update PrivateRoute.jsx

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -35,7 +35,7 @@ const PrivateRoute = ({ element: Element, ...rest }) => {
   useEffect(() => {
     const fetchAuthenticationStatus = async () => {
       try {
-        const response = await axios.get('https://dashboard.pinesphere.co.in/api/user/register/'); // Endpoint to check authentication status
+        const response = await axios.get('https://dashboard.pinesphere.com/api/user/register/'); // Endpoint to check authentication status
         setIsAuthenticated(response.data.isAuthenticated);
       } catch (error) {
         console.error('Error fetching authentication status:', error);
